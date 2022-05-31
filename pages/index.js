576154cedcc1d05ce0a1a4bb55c37b2e66be8ad3
Juan Blancasm <juanblancase@gmail.com>
2022-05-31 13:44:23 -0300

    add sobre.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+
 function Home() {
-    return <div>Home 2</div>
+    return (
+    <div>
+        <h1>Home</h1>
+        <a href="/sobre"> Acessar página Sobre</a>
+    </div>)
 }
 
 export default Home     
