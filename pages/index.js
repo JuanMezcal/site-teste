8dbe9483eb0bdd0f13701c28adefe68fc88a559f
Juan Blancasm <juanblancase@gmail.com>
2022-05-31 13:55:42 -0300

    teste de links

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 
+import Link from 'next/link';
+
 function Home() {
     return (
     <div>
         <h1>Home</h1>
+        <Link>
         <a href="/sobre"> Acessar página Sobre</a>
+        </Link>
     </div>)
 }
 
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,12 @@
 
+import Link from 'next/link';
 function Sobre() {
     return (
     <div>
         <h1>Home</h1>
+        <Link>
         <a href="/"> Acessar página Home</a>
+        </Link>
     </div>)
 }
 
